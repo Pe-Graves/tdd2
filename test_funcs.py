e57348efc47eb63ff0d45c4e1ee511bb00ec66fa
Paul-Edouard <pauledouardg@gmail.com>
2022-01-20 22:08:03 +0100

    tests miroir v3

--- test_funcs.py
+++ test_funcs.py
@@ -5,10 +5,12 @@ class TestFuncs(unittest.TestCase):
     
     def testMirroir(self):
         self.assertEqual(funcs.miroir("azerty",3),"azerreza")
-        self.assertEqual(funcs.miroir("123456789",5),"1234554321")
+        self.assertEqual(funcs.miroir("123456789",5),"123456654321")
         self.assertEqual(funcs.miroir("a",2),"")
         self.assertEqual(funcs.miroir(" ",10)," ")
         self.assertEqual(funcs.miroir("",10),"")
+        self.assertEqual(funcs.miroir("&",0),"&&")
+
 
 
 if __name__ == '__main__':
