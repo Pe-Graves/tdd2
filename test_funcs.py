902c3308e8dda76b3f748b6b3fcc5dbeeb1587e3
Paul-Edouard <pauledouardg@gmail.com>
2022-01-20 20:30:47 +0100

    tests miroir

---
+++ test_funcs.py
@@ -0,0 +1,14 @@
+import funcs
+import unittest
+
+class TestFuncs(unittest.TestCase):
+    
+    def testMirroir(self):
+        self.assertEqual(funcs.miroir("azerty",3),"azerreza")
+        self.assertEqual(funcs.miroir("123456789",5),"1234554321")
+        self.assertEqual(funcs.miroir("a",2),"")
+        self.assertEqual(funcs.miroir(" ",10)," ")
+
+
+if __name__ == '__main__':
+	unittest.main()
