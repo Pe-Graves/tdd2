f5c7332f9ec5058375e7448680ee5167ab515825
Paul-Edouard <pauledouardg@gmail.com>
2022-01-21 19:47:33 +0100

    approximation

--- funcs.py
+++ funcs.py
@@ -1,3 +1,24 @@
-def approximation(fonction, ordre, point):
-    return -1
+def fonction1(x):
+    return 5*x**2
 
+def fonction2(x):
+    return 3*x
+
+def fonction3(x):
+    return 1/x
+
+
+def approximation(fonction, ordre, point):
+
+    if ordre > 0 and ordre < 1 and point != 0:
+        res = fonction(point)
+        multi_ordre = 1 / ordre
+        res *= multi_ordre
+        res = int(res)
+        return res*ordre
+    
+    if ordre >= 1 and point != 0:
+        return fonction(point)
+
+    if ordre <= 0 or point == 0:
+        return 0
