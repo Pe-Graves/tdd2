a09bcdf9e2c3a609ae8f110964074fd98e1b3121
Paul-Edouard <pauledouardg@gmail.com>
2022-01-21 09:39:56 +0100

    tests v2

--- test_funcs.py
+++ test_funcs.py
@@ -12,8 +12,8 @@ class TestFuncs(unittest.TestCase):
         self.assertEqual(funcs.deriveeSeconde(["adad","azfdnaf","zfh","ozlad"]),[0.0])
         self.assertEqual(funcs.deriveeSeconde(['a','b','r','i']),[0.0])
         self.assertEqual(funcs.deriveeSeconde([1,2,3,4,5]),[0.0])
-        self.assertEqual(funcs.deriveeSeconde([1.0,1.0,1.0,1.0]),[0.0])
-        self.assertEqual(funcs.deriveeSeconde([1.0,2.0,2.0,4.0]),[0.75])
+        self.assertEqual(funcs.deriveeSeconde([1.0,1.0,1.0,1.0]),[0.0,0.0])
+        self.assertEqual(funcs.deriveeSeconde([1.0,2.0,2.0,4.0]),[-0.25,0.5])
 
 
 if __name__ == '__main__':
