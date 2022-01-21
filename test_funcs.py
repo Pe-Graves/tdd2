9b1f6cc8aed90e884f5a399ca9c8d66729cb4056
Paul-Edouard <pauledouardg@gmail.com>
2022-01-21 09:30:45 +0100

    test Dérivée Seconde

--- test_funcs.py
+++ test_funcs.py
@@ -4,6 +4,17 @@ import unittest
 class TestFuncs(unittest.TestCase):
     
     def testDeriveeSeconde(self):
+        self.assertEqual(funcs.deriveeSeconde([],[0.0]))
+        self.assertEqual(funcs.deriveeSeconde([0.0],[0.0]))
+        self.assertEqual(funcs.deriveeSeconde([0.0,0.0],[0.0]))
+        self.assertEqual(funcs.deriveeSeconde([0.0,0.0,0.0],[0.0]))
+        self.assertEqual(funcs.deriveeSeconde([0.0,0.0,0.0],[0.0]))
+        self.assertEqual(funcs.deriveeSeconde(["adad","azfdnaf","zfh","ozlad"],[0.0]))
+        self.assertEqual(funcs.deriveeSeconde(['a','b','r','i'],[0.0]))
+        self.assertEqual(funcs.deriveeSeconde([1,2,3,4,5],[0.0]))
+        self.assertEqual(funcs.deriveeSeconde([1.0,1.0,1.0,1.0],[0.0]))
+        self.assertEqual(funcs.deriveeSeconde([1.0,2.0,2.0,4.0],[0.75]))
+
 
 if __name__ == '__main__':
 	unittest.main()
