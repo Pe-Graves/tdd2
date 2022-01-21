9dc6d7f79bc9bd8ab5c8a497c17ec7cf1fddaa34
Paul-Edouard <pauledouardg@gmail.com>
2022-01-21 19:46:54 +0100

    test approximation V2

--- test_funcs.py
+++ test_funcs.py
@@ -4,11 +4,34 @@ import unittest
 class TestFuncs(unittest.TestCase):
     
     def testApproximationFunction(self):
-        self.assertEqual(funcs.approximation(funcs.fonction,0.4253,0.01),7.96)
-        self.assertEqual(funcs.approximation(funcs.fonction,0.234,0.001),4.92)
-        self.assertEqual(funcs.approximation(funcs.fonction,1,1),25)
-        self.assertEqual(funcs.approximation(funcs.fonction,0,0.001),0)
-        self.assertEqual(funcs.approximation(funcs.fonction,0.234,0),0)
+
+        self.assertEqual(funcs.approximation(funcs.fonction1,0.01,0),0)
+        self.assertEqual(funcs.approximation(funcs.fonction1,0.001,0.4253),0.904)
+        self.assertEqual(funcs.approximation(funcs.fonction1,0.1,1.23),7.5)
+
+        self.assertEqual(funcs.approximation(funcs.fonction2,0.01,0),0)
+        self.assertEqual(funcs.approximation(funcs.fonction2,0.01,0.4253),1.27)
+        self.assertEqual(funcs.approximation(funcs.fonction2,0.1,1.23),3.6)
+
+        self.assertEqual(funcs.approximation(funcs.fonction3,0.01,0),0)
+        self.assertEqual(funcs.approximation(funcs.fonction3,0.01,0.4253),2.35)
+        self.assertEqual(funcs.approximation(funcs.fonction3,0.1,1.23),0.8)
+
+        self.assertEqual(funcs.approximation(funcs.fonction1,0,1),0)
+        self.assertEqual(funcs.approximation(funcs.fonction2,0,1),0)
+        self.assertEqual(funcs.approximation(funcs.fonction3,0,1),0)
+
+        self.assertEqual(funcs.approximation(funcs.fonction1,1,1),5)
+        self.assertEqual(funcs.approximation(funcs.fonction2,1,1),3)
+        self.assertEqual(funcs.approximation(funcs.fonction3,1,1),1)
+
+        self.assertEqual(funcs.approximation(funcs.fonction1,1.23,1),5)
+        self.assertEqual(funcs.approximation(funcs.fonction2,1.23,1),3)
+        self.assertEqual(funcs.approximation(funcs.fonction3,1.23,1),1)
+
+        self.assertEqual(funcs.approximation(funcs.fonction1,-1.23,1),0)
+        self.assertEqual(funcs.approximation(funcs.fonction2,-1.23,1),0)
+        self.assertEqual(funcs.approximation(funcs.fonction3,-1.23,1),0)
 
 
 
