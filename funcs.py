6a2b6fb71cfbaed191416badb8e32f5b03dd0d08
Paul-Edouard <pauledouardg@gmail.com>
2022-01-21 09:50:45 +0100

    Initial Commit

--- funcs.py
+++ funcs.py
@@ -1,17 +1,3 @@
-def deriveeSeconde(floatListe):
-    tab = []
-    res = []
-    temp = 2 # interval de temps régulier
-    for i in floatListe:
-        if type(i) != float:
-            return [0.0]
-    if(floatListe == [] or len(floatListe) < 3):
-        return [0.0]
-    for i in range(len(floatListe)-1):
-        tab.append((floatListe[i+1]-floatListe[i])/temp)
-    if(tab == [] or len(tab) < 2):
-        return [0.0]
-    for j in range(len(tab)-1):
-        res.append((tab[j+1]-tab[j])/temp)
-    return res
+def deriveeSeconde(fonction, ordre, point):
+    return -1
 
