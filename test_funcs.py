2ba23c4e6013e21043566dc78273b1bf3dc2c746
Paul-Edouard <pauledouardg@gmail.com>
2022-01-24 18:09:07 +0100

    correction erreur

--- test_funcs.py
+++ test_funcs.py
@@ -7,7 +7,7 @@ class TestFuncs(unittest.TestCase):
         self.assertEqual(funcs.miroir("azerty",3),"azerreza")
         self.assertEqual(funcs.miroir("123456789",5),"123456654321")
         self.assertEqual(funcs.miroir("a",2),"")
-        self.assertEqual(funcs.miroir(" ",10)," ")
+        self.assertEqual(funcs.miroir(" ",10),"")
         self.assertEqual(funcs.miroir("",10),"")
         self.assertEqual(funcs.miroir("&",0),"&&")
         self.assertEqual(funcs.miroir('a',0),'aa')
