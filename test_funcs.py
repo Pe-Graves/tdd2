e9b4d1ecdeb2104ea2831ea05d73f9ce3e7443ba
Paul-Edouard <pauledouardg@gmail.com>
2022-01-24 18:15:29 +0100

    split tests

--- test_funcs.py
+++ test_funcs.py
@@ -12,6 +12,7 @@ class TestFuncs(unittest.TestCase):
         self.assertEqual(funcs.deriveeSeconde(["adad","azfdnaf","zfh","ozlad"]),[0.0])
         self.assertEqual(funcs.deriveeSeconde(['a','b','r','i']),[0.0])
         self.assertEqual(funcs.deriveeSeconde([1,2,3,4,5]),[0.0])
+        
         self.assertEqual(funcs.deriveeSeconde([1.0,1.0,1.0,1.0]),[0.0,0.0])
         self.assertEqual(funcs.deriveeSeconde([1.0,2.0,2.0,4.0]),[-0.25,0.5])
 
