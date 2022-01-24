f097a290d6a45cfe3c476da6b289109ad2b1f79c
Paul-Edouard <pauledouardg@gmail.com>
2022-01-24 18:13:23 +0100

    split and add tests

--- test_funcs.py
+++ test_funcs.py
@@ -5,14 +5,16 @@ class TestFuncs(unittest.TestCase):
     
     def testDerivee(self):
         self.assertEqual(funcs.derivee([2.0,1.0,4.0,3.0]),[-0.5,1.5,-0.5])
-        self.assertEqual(funcs.derivee([]),[0.0])
         self.assertEqual(funcs.derivee([2.0,2.0,1.0]),[0.0,-0.5])
-        self.assertEqual(funcs.derivee([0.0]),[0.0])
         self.assertEqual(funcs.derivee([0.0,0.0]),[0.0])
         self.assertEqual(funcs.derivee([0.0,1.0]),[0.5])
+
         self.assertEqual(funcs.derivee(["a","b","d","zd"]),[0.0])
         self.assertEqual(funcs.derivee([1,2,3,4,4]),[0.0])
         self.assertEqual(funcs.derivee(['a','b','m']),[0.0])
+        self.assertEqual(funcs.derivee(['&','[[','||']),[0.0])
+        self.assertEqual(funcs.derivee([]),[0.0])
+        self.assertEqual(funcs.derivee([0.0]),[0.0])
 
 
 
