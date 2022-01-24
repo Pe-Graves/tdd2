2d331c56e5a8154cd94ed3e73d933d574498a3ed
Paul-Edouard <pauledouardg@gmail.com>
2022-01-24 19:19:44 +0100

    type/val ordre

--- funcs.py
+++ funcs.py
@@ -10,15 +10,25 @@ def fonction3(x):
 
 def approximation(fonction, ordre, point):
 
-    if ordre > 0 and ordre < 1 and point != 0:
+    if type(ordre) != float or type(point) != float or ordre <= 0.0 or point == 0.0:
+        return 0
+
+    if ordre >= 1.0:
+        return fonction(point)
+    
+    ordre_inverse = 1.0/ordre
+    verif_ordre = ordre*ordre_inverse
+
+    if ordre_inverse%10 != 0:
+        return 0
+
+    if ordre > 0.0 and ordre < 1.0 and point != 0.0:
         res = fonction(point)
-        multi_ordre = 1 / ordre
+        multi_ordre = 1/ordre
         res *= multi_ordre
         res = int(res)
         return res*ordre
     
-    if ordre >= 1 and point != 0:
-        return fonction(point)
+    
 
-    if ordre <= 0 or point == 0:
-        return 0
+    
